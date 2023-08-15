bd286e2063e59fb47b2c01318f0a91fe4ce02d23
DanYunak <danyloyunak2006@gmail.com>
2023-08-15 21:57:47 +0300

    Fixed bug

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ import router from './routes/index.js'
 import errorMiddleware from './middlewares/errorMiddleware.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
 
 dotenv.config()
 
 const PORT = process.env.PORT || 7000
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
@@ -20,6 +25,12 @@ app.use(cors({
 }))
 
 app.use('/api', router)
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
 
 app.use(errorMiddleware) // should be the last in the list of middlewares
 
